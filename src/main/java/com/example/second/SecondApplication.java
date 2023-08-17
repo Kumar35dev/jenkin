d1476d21d718b4c514f1dae6238ec1d261938f5b
Kumar35dev <udaykumarpokanati@gmail.com>
2023-08-17 14:56:05 +0530

    first changes to the jenkin repo

--- src/main/java/com/example/second/SecondApplication.java
+++ src/main/java/com/example/second/SecondApplication.java
@@ -2,12 +2,17 @@ package com.example.second;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SecondApplication {
+public class SecondApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecondApplication.class, args);
 	}
-
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SecondApplication.class);
+	}
 }
